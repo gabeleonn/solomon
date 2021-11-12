894230b16e1bfe691a9b5a4f08f1fca191a6e680
Gabriel Leon <gableonn@gmail.com>
2021-11-12 20:29:43 -0300

    test: should call usecase to return verse data

--- src/api/words/controllers/get-single-verse/controller.spec.ts
+++ src/api/words/controllers/get-single-verse/controller.spec.ts
@@ -1,8 +1,53 @@
 import { GetSingleVerseController } from './controller';
+import { IGetSingleVerse } from '@/api/words/usecases/get-single-verse/interface';
+import { Reference } from '@/api/words/models/reference';
+import { Word } from '@/api/words/models/word';
+import { IController } from '@/common/protocols/controller';
+
+const word: Word = {
+  definition: 'definition',
+  language: 'greek',
+  original: 'original',
+  parsing_long: 'parsing_long',
+  parsing_short: 'parsing_short',
+  reference: {
+    book: 'genesis',
+    chapter: 1,
+    verse: 1,
+  },
+  sorting: 1,
+  strong: 1,
+  translation: 'translation',
+  translit: 'translit',
+};
+
+function makeGetSingleVerseStub(): IGetSingleVerse {
+  class GetSingleVErseStub implements IGetSingleVerse {
+    async get(reference: Reference): Promise<Word> {
+      return await new Promise(resolve => resolve(word));
+    }
+  }
+
+  return new GetSingleVErseStub();
+}
+
+interface SutType {
+  getSingleVerseStub: IGetSingleVerse;
+  sut: IController;
+}
+
+function makeSut(): SutType {
+  const getSingleVerseStub = makeGetSingleVerseStub();
+  const sut = new GetSingleVerseController(getSingleVerseStub);
+  return {
+    sut,
+    getSingleVerseStub,
+  };
+}
 
 describe(`Get single verse controller`, () => {
   it(`should return 400 if book field is not present`, async () => {
-    const sut = new GetSingleVerseController();
+    const { sut } = makeSut();
 
     const response = await sut.handle({
       data: {
@@ -19,7 +64,7 @@ describe(`Get single verse controller`, () => {
   });
 
   it(`should return 400 if chapter field is not present`, async () => {
-    const sut = new GetSingleVerseController();
+    const { sut } = makeSut();
 
     const response = await sut.handle({
       data: {
@@ -36,7 +81,7 @@ describe(`Get single verse controller`, () => {
   });
 
   it(`should return 400 if verse field is not present`, async () => {
-    const sut = new GetSingleVerseController();
+    const { sut } = makeSut();
 
     const response = await sut.handle({
       data: {
@@ -49,6 +94,25 @@ describe(`Get single verse controller`, () => {
     expect(response).toEqual({
       status: 400,
       message: 'O parametro "verse" é necessário.',
+    });
+  });
+
+  it(`should call usecase with proper data`, async () => {
+    const { sut, getSingleVerseStub } = makeSut();
+    const spy = jest.spyOn(getSingleVerseStub, 'get');
+
+    await sut.handle({
+      data: {
+        book: 'genesis',
+        chapter: 1,
+        verse: 1,
+      },
+    });
+
+    expect(spy).toBeCalledWith({
+      book: 'genesis',
+      chapter: 1,
+      verse: 1,
     });
   });
 });
