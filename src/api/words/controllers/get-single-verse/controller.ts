150a9bb0888d427c12d85c9d1a7e2fa0aaf48a52
Gabriel Leon <gableonn@gmail.com>
2021-11-12 19:34:45 -0300

    feat: add get single verse validation of chapter field

--- src/api/words/controllers/get-single-verse/controller.ts
+++ src/api/words/controllers/get-single-verse/controller.ts
@@ -5,8 +5,15 @@ import {
 } from '@/common/protocols/controller';
 
 export class GetSingleVerseController implements IController {
-  async handle(request: IRequest): Promise<IResponse> {
-    console.log(request);
+  async handle({ data }: IRequest): Promise<IResponse> {
+    for (const field of ['book', 'chapter', 'verse']) {
+      if (!data[field]) {
+        return {
+          status: 400,
+          message: `O parametro "${field}" é necessário.`,
+        };
+      }
+    }
     return await new Promise(resolve =>
       resolve({ status: 400, message: 'O parametro "book" é necessário.' }),
     );
