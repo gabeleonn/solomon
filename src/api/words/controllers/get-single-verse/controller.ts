14110cc54edab0e69ebd3030fcdc6217f5fa4b02
Gabriel Leon <gableonn@gmail.com>
2021-11-12 23:38:40 -0300

    feat: add try catch block on controller

--- src/api/words/controllers/get-single-verse/controller.ts
+++ src/api/words/controllers/get-single-verse/controller.ts
@@ -5,8 +5,8 @@ import {
   IRequest,
   IResponse,
 } from '@/common/protocols/controller';
-import { MissingParamError } from '@/common/errors';
-import { badRequest, ok } from '@/common/helpers/http';
+import { MissingParamError, ServerError } from '@/common/errors';
+import { badRequest, ok, serverError } from '@/common/helpers/http';
 
 export class GetSingleVerseController implements IController {
   private readonly getSingleVerse: IGetSingleVerse;
@@ -16,16 +16,24 @@ export class GetSingleVerseController implements IController {
   }
 
   async handle({ data }: IRequest): Promise<IResponse> {
-    for (const field of ['book', 'chapter', 'verse']) {
-      if (!data[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      for (const field of ['book', 'chapter', 'verse']) {
+        if (!data[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
+      const { book, chapter, verse } = data;
+
+      const singleVerse = await this.getSingleVerse.get({
+        book,
+        chapter,
+        verse,
+      });
+
+      return ok(singleVerse);
+    } catch (error) {
+      return serverError(new ServerError(error.stack));
     }
-
-    const { book, chapter, verse } = data;
-
-    const singleVerse = await this.getSingleVerse.get({ book, chapter, verse });
-
-    return ok(singleVerse);
   }
 }
