da78ae8371e71d98bce5a0ad0bd413c418cb814e
Gabriel Leon <gableonn@gmail.com>
2021-11-14 15:47:01 -0300

    chore: resolve linting probs

--- data/generate-db.ts
+++ data/generate-db.ts
@@ -1,29 +1,34 @@
-import fs from 'fs'
-import { MongoClient } from 'mongodb'
-import env from '../src/config/env'
+import fs from 'fs';
+import { MongoClient } from 'mongodb';
+import env from '../src/config/env';
 
+const client = new MongoClient(env.mongoUrl);
 
-const client = new MongoClient(env.mongoUrl)
+async function generate(): Promise<void> {
+  await client.connect();
+  const db = client.db('words-api');
+  const collection = db.collection('words');
+  const files = fs
+    .readdirSync('./data/books', { encoding: 'utf8', withFileTypes: true })
+    .filter(dir => /.json$/gi.test(dir.name));
 
-async function generate() {
-    await client.connect()
-    const db = client.db('words-api')
-    const collection = db.collection('words')
-    const files = fs.readdirSync('./data/books', { encoding: 'utf8', withFileTypes: true }).filter(dir => /.json$/gi.test(dir.name))
+  const words = [];
+  files.forEach((each): void => {
+    const file = fs.readFileSync(`./data/books/${each.name}`, {
+      encoding: 'utf8',
+    });
+    const json = JSON.parse(file);
+    json.forEach((word: any) => words.push(word));
+  });
 
-    const words = []
-    files.forEach(async each => {
-            const file = fs.readFileSync(`./data/books/${each.name}`, { encoding: 'utf8' })
-            const json = JSON.parse(file)
-            json.map(word => words.push(word))
-    })
+  await collection.insertMany(words);
 
-    await collection.insertMany(words)
-
-    console.log(await collection.find({  book: "John", chapter: "2", verse: "1" }).toArray())
+  console.log(
+    await collection.find({ book: 'John', chapter: '2', verse: '1' }).toArray(),
+  );
 }
 
 generate()
-    .then()
-    .catch(e => console.log(e.message))
-    .finally(() => client.close())
+  .then()
+  .catch(e => console.log(e.message))
+  .finally(async () => await client.close());
--- data/generate-json.ts
+++ data/generate-json.ts
@@ -1,167 +1,172 @@
-import fs from 'fs'
-import xlsx, { CellValue, Row, Worksheet } from 'exceljs'
+import fs from 'fs';
+import xlsx, { CellValue, Row, Worksheet } from 'exceljs';
 
-const wb = new xlsx.Workbook()
+const wb = new xlsx.Workbook();
 // const stream = fs.createReadStream('data/original.xlsx')
 
 interface IObject {
-    [key: string]: any[]
+  [key: string]: any[];
 }
 const Objeto: IObject = {
-    genesis: [],
-    exodus: [],
-    leviticus: [],
-    numbers: [],
-    deuteronomy: [],
-    joshua: [],
-    judges: [],
-    ruth: [],
-    '1_samuel': [],
-    '2_samuel': [],
-    '1_kings': [],
-    '2_kings': [],
-    '1_chronicles': [],
-    '2_chronicles': [],
-    ezra: [],
-    nehemiah: [],
-    esther: [],
-    job: [],
-    psalm: [],
-    proverbs: [],
-    ecclesiastes: [],
-    song_of_solomon: [],
-    isaiah: [],
-    jeremiah: [],
-    lamentations: [],
-    ezekiel: [],
-    daniel: [],
-    hosea: [],
-    joel: [],
-    amos: [],
-    obadiah: [],
-    jonah: [],
-    micah: [],
-    nahum: [],
-    habakkuk: [],
-    zephaniah: [],
-    haggai: [],
-    zechariah: [],
-    malachi: [],
-    matthew: [],
-    mark: [],
-    luke: [],
-    john: [],
-    acts: [],
-    romans: [],
-    '1_corinthians': [],
-    '2_corinthians': [],
-    galatians: [],
-    ephesians: [],
-    philippians: [],
-    colossians: [],
-    '1_thessalonians': [],
-    '2_thessalonians': [],
-    '1_timothy': [],
-    '2_timothy': [],
-    titus: [],
-    philemon: [],
-    hebrews: [],
-    james: [],
-    '1_peter': [],
-    '2_peter': [],
-    '1_john': [],
-    '2_john': [],
-    '3_john': [],
-    jude: [],
-    revelation: []
-}
+  genesis: [],
+  exodus: [],
+  leviticus: [],
+  numbers: [],
+  deuteronomy: [],
+  joshua: [],
+  judges: [],
+  ruth: [],
+  '1_samuel': [],
+  '2_samuel': [],
+  '1_kings': [],
+  '2_kings': [],
+  '1_chronicles': [],
+  '2_chronicles': [],
+  ezra: [],
+  nehemiah: [],
+  esther: [],
+  job: [],
+  psalm: [],
+  proverbs: [],
+  ecclesiastes: [],
+  song_of_solomon: [],
+  isaiah: [],
+  jeremiah: [],
+  lamentations: [],
+  ezekiel: [],
+  daniel: [],
+  hosea: [],
+  joel: [],
+  amos: [],
+  obadiah: [],
+  jonah: [],
+  micah: [],
+  nahum: [],
+  habakkuk: [],
+  zephaniah: [],
+  haggai: [],
+  zechariah: [],
+  malachi: [],
+  matthew: [],
+  mark: [],
+  luke: [],
+  john: [],
+  acts: [],
+  romans: [],
+  '1_corinthians': [],
+  '2_corinthians': [],
+  galatians: [],
+  ephesians: [],
+  philippians: [],
+  colossians: [],
+  '1_thessalonians': [],
+  '2_thessalonians': [],
+  '1_timothy': [],
+  '2_timothy': [],
+  titus: [],
+  philemon: [],
+  hebrews: [],
+  james: [],
+  '1_peter': [],
+  '2_peter': [],
+  '1_john': [],
+  '2_john': [],
+  '3_john': [],
+  jude: [],
+  revelation: [],
+};
 
-function spreadReference(sheet: Worksheet) {
-    let lastValue = null
+function spreadReference(sheet: Worksheet): void {
+  let lastValue = null;
 
-    for(let i = 2; i <= sheet.rowCount; i++) {
-        const row = sheet.getRow(i)
-        const cell = row.getCell(11).value
-        if(cell) {
-            lastValue = cell
-        } else {
-            sheet.getRow(i).getCell(11).value = lastValue
-            row.commit()
-        }      
+  for (let i = 2; i <= sheet.rowCount; i++) {
+    const row = sheet.getRow(i);
+    const cell = row.getCell(11).value;
+    if (cell) {
+      lastValue = cell;
+    } else {
+      sheet.getRow(i).getCell(11).value = lastValue;
+      row.commit();
     }
+  }
 }
 
-function toObject(header: CellValue[] | { [key: string]: CellValue; }, row: Row, reference: string[]) {
-    const object: { [key: string]: CellValue | string[] | Object } = {}
-    if(Array.isArray(row.values)) {
-        row.values.map((value, index) => {
-            if(index > 0) {
-                if(Array.isArray(header)) {
-                    const identifier = header[index]?.toString()
+function toObject(
+  header: CellValue[] | { [key: string]: CellValue },
+  row: Row,
+  reference: string[],
+): { [key: string]: CellValue | string[] | Object } {
+  const object: { [key: string]: CellValue | string[] | Object } = {};
+  if (Array.isArray(row.values)) {
+    row.values.forEach((value, index) => {
+      if (index > 0) {
+        if (Array.isArray(header)) {
+          const identifier = header[index]?.toString();
 
-                    if(identifier) {
-                        if(identifier === 'cross_reference') {
-                            object[identifier] = value.toString().replace('(', '').replace(')', '').split(';').map(each => each.trim())
-                        }  else if(identifier === 'reference') {
-                            object['book'] = reference[2]
-                            object['chapter'] = reference[3]
-                            object['verse'] = reference[4]
-                        } else if(identifier === 'language') {
-                            object[identifier] = value.toString().toLowerCase()
-                        } else {
-                            object[identifier] = typeof value === 'string' ? value.toString().trim() : value
-                        }
-                    }
-
-                }
+          if (identifier) {
+            if (identifier === 'cross_reference') {
+              object[identifier] = value
+                .toString()
+                .replace('(', '')
+                .replace(')', '')
+                .split(';')
+                .map(each => each.trim());
+            } else if (identifier === 'reference') {
+              object.book = reference[2];
+              object.chapter = reference[3];
+              object.verse = reference[4];
+            } else if (identifier === 'language') {
+              object[identifier] = value.toString().toLowerCase();
+            } else {
+              object[identifier] =
+                typeof value === 'string' ? value.toString().trim() : value;
             }
-            
-        })
-    }
-    return object
-}
-
-async function readFile() {
-    
-
-    const file = await wb.xlsx.readFile('data/original.xlsx')
-    const sheet = file.getWorksheet('data')
-    const regex = /^(\d?\s?([a-z]+\s?)+)\s(\d{1,3}):(\d{1,3})$/gi
-
-    const header = sheet.getRow(1).values;
-
-    spreadReference(sheet)
-
-    let debug = 0;
-
-    try {
-        for(let i = 2; i <= sheet.rowCount; i++) {
-            // columnName: value
-            debug = i;
-            const row = sheet.getRow(i)
-            
-            const matches = [...row.getCell(11).text.matchAll(regex)][0];
-            const book = matches[1].toLowerCase().trim().replace(/\s/gi, '_')
-            Objeto[book].push(toObject(header, row, matches))
+          }
         }
-        writeFiles(Objeto);
-    } catch(e) {
-        console.log(e)
-        console.log(sheet.getRow(debug).values)
-    }   
+      }
+    });
+  }
+  return object;
 }
 
-function writeFiles(output: IObject) {
-    // console.log(Object.keys(output))
+async function readFile(): Promise<void> {
+  const file = await wb.xlsx.readFile('data/original.xlsx');
+  const sheet = file.getWorksheet('data');
+  const regex = /^(\d?\s?([a-z]+\s?)+)\s(\d{1,3}):(\d{1,3})$/gi;
 
-    for (let key of Object.keys(output)) {
-        fs.writeFileSync(`data/books/${key}.json`, JSON.stringify(output[key]))
+  const header = sheet.getRow(1).values;
+
+  spreadReference(sheet);
+
+  let debug = 0;
+
+  try {
+    for (let i = 2; i <= sheet.rowCount; i++) {
+      // columnName: value
+      debug = i;
+      const row = sheet.getRow(i);
+
+      const matches = [...row.getCell(11).text.matchAll(regex)][0];
+      const book = matches[1].toLowerCase().trim().replace(/\s/gi, '_');
+      Objeto[book].push(toObject(header, row, matches));
     }
-    
+    writeFiles(Objeto);
+  } catch (e) {
+    console.log(e);
+    console.log(sheet.getRow(debug).values);
+  }
+}
+
+function writeFiles(output: IObject): void {
+  // console.log(Object.keys(output))
+
+  for (const key of Object.keys(output)) {
+    fs.writeFileSync(`data/books/${key}.json`, JSON.stringify(output[key]));
+  }
 }
 
 // function writeFile(output: IObject) {
 //     fs.writeFileSync(`data/books/bible.json`, JSON.stringify(output))
 // }
 
-readFile();
+readFile().catch(e => console.log(e));
